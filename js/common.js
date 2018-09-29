a60fb4b57153335ddf3dc00d0a2740dfa74d0caa
maralbars <maralbars@gmail.com>
2018-09-29 18:56:35 +0600

    Praha

--- js/common.js
+++ js/common.js
@@ -1,41 +1,184 @@
-$(function() {
+// $(function() {
+
+	var game_audio = {
+		intro: new Audio('../music/intro.mp3'),
+		hover_btn: new Audio('../music/click3.wav')
+	}
 
 
-	$(".questions").html(
-		$(".questions").children("li").sort(function (a, b) {
-			return $(a).text().toUpperCase().localeCompare(
-				$(b).text().toUpperCase());
-		})
-		);
 
-	$(".questions a").animatedModal({
-		color: '#4e4e4e',
-		animatedIn: 'fadeInRight',
-		animatedOut: 'fadeOutLeft'
-	});
-
-	$(document).on('click', '.questions a', function(e){
-		var answer = '#answer-' + $(this).data('question');
-		$('.answers').hide();
-		$(answer).show();
-	});
-
-	$(document).on('click', '.questions button.del', function(e){
-		$(this).closest('li').remove();
-		if ( $('.questions button.del').length < 4 )
-			$('.questions button.del').hide();
-	});
-
-	$(document).on('click', '.questions button.good', function(e){
-		$(this).closest('li').addClass('success').find('button.del').remove();
-		if ( $('.questions .success').length > 3 ){
-			$('.questions button.del').closest('li').each(function(i, item){
-				if ( !$(item).hasClass('success') ) {
-					$(item).hide();
-				}
+	var Game = {
+		init: function(){
+			shadows.on();
+			game_audio.intro.play();
+			this.intro();
+			shadows.off();
+			Game.sound.init();
+		},
+		intro: function(target){
+			var next_step,
+				game = this,
+				target = game.state.check();
+			$('section.intro').addClass('active');
+			next_step = ( target === 'level1' ) ? target : 'select_level';
+			$(document).on('click', '#start-game', function(){
+				shadows.on_revert();
+				game[next_step].init(target);
+				shadows.off_revert(function(){
+					$(game[next_step].selector).show();
+					$('section.intro').removeClass('active');
+				});
 			});
+			$('#start-game, .sound, .shover').on('mouseenter', function(){
+				game_audio.hover_btn.play();
+			});
+		},
+		level1: {
+			selector: 'section.level-1',
+			init: function(){
+				console.log('level 1 inited');
+				// Пошел за кофе
+				$(document).on('click', '.coffee, .want-coffee, .standart-pass, .banya, .whos, .danu', function(){
+					shadows.on_revert();
+					setTimeout(cut_scene.level.one.fail, 1500);
+				});
+				// Нажал открыть
+				$(document).on('click', '.closed-laptop button, .closed-laptop img', function(){
+					$('.closed-laptop').hide();
+					$('.laptop-open').show();
+				});
+				$(document).on('click', '.settings', function(){
+					if ( !$(this).hasClass('disabled') ) {
+						$('.folder, .settings').addClass('disabled');
+						$('.password').show();
+						$('.want-coffee').hide();
+					}
+				});
+				$(document).on('click', 'button.cancel-pass, .pass-close', function(){
+					$('.folder, .settings').removeClass('disabled');
+					$('.password').hide();
+					$('.want-coffee').show();
+				});
+				$(document).on('input','input[type="password"]', function(){
+					var password = $(this).val();
+					console.log(password.length);
+					if ( password.length > 8 ) {
+						$('button.save-pass').prop('disabled', false);
+					} else {
+						$('button.save-pass').prop('disabled', true);
+					}
+				});
+				$(document).on('click', '.folder', function(){
+					if ( !$(this).hasClass('disabled') ) {
+						$('.folder, .settings').addClass('disabled');
+						$('.folder-opened').show();
+						$('.want-coffee').hide();
+					}
+				});
+				$(document).on('click', '.folder-close', function(){
+					$('.folder, .settings').removeClass('disabled');
+					$('.folder-opened, .danu').hide();
+					$('.want-coffee').show();
+				});
+				$(document).on('click', '.photos', function(){
+					$('.coffee').hide();
+					$('.want-coffee').hide();
+					// $('.folder-close').hide();
+					$('.whos').show();
+					setTimeout(function(){
+						$('.danu').show();
+					}, 1200);
+				});
+
+			}
+		},
+		level2: {
+			init: function(){
+				console.log('level 2 inited');
+			}
+		},
+		select_level: {
+			init: function() {
+				console.log('Level select inited');
+			}
+		},
+		state: {
+			current: '',
+			check: function(){
+				var temp = '';
+				this.current = getCookie('game_state') || 'level1';
+				this.save(this.current);
+				return this.current;
+			},
+			save: function(level){
+				setCookie('game_state', level, {
+					expires: 31104000
+				});
+			},
+			clear: function(){
+				deleteCookie('game_state');
+			}
+		},
+		sound: {
+			on: function(){
+				for ( sound in game_audio ) {
+					game_audio[sound].volume = 1;
+					$('.sound').removeClass('mute');
+					// var timer = setInterval(function(){
+					// 	if (game_audio[sound].volume != 1){
+					// 		game_audio[sound].volume = game_audio[sound].volume + 0.05
+					// 	} else {
+					// 		clearInterval(timer);
+					// 	}
+					// }, 50)
+				}
+			},
+			off: function(){
+				$('.sound').addClass('mute');
+				for ( sound in game_audio ) {
+					game_audio[sound].volume = 0;
+				}
+			},
+			init: function(){
+				if (typeof getCookie('game_sound') == 'undefined') {
+					setCookie('game_sound', true, { expires: 31104000 });
+				}
+				if ( getCookie('game_sound') == 'true' ) {
+					console.log('sound On');
+					this.on();
+				} else {
+					console.log('sound Off');
+					this.off();
+				}
+			}
 		}
+	}
+
+	Game.init();
+	$(document).on('click', '.sound', function(){
+		var volume = ( getCookie('game_sound') == 'true' ) ? false : true;
+		setCookie('game_sound', volume, { expires: 31104000 });
+		Game.sound.init();
 	});
 
+	$(document).on('click', '.repeat', function(){
+		location.reload();
+	});
 
-});
+	var cut_scene = {
+		level: {
+			one: {
+				fail: function(){
+					$('body').addClass('noscroll');
+					$('.cut-scene').show();
+					shadows.on_revert();
+				}
+			}
+		}
+	}
+
+
+
+// });
+
+
---
+++ js/functions.js
@@ -0,0 +1,64 @@
+function getCookie(name) {
+	var matches = document.cookie.match(new RegExp(
+		"(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+		));
+	return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+function setCookie(name, value, options) {
+	options = options || {};
+
+	var expires = options.expires;
+
+	if (typeof expires == "number" && expires) {
+		var d = new Date();
+		d.setTime(d.getTime() + expires * 1000);
+		expires = options.expires = d;
+	}
+	if (expires && expires.toUTCString) {
+		options.expires = expires.toUTCString();
+	}
+
+	value = encodeURIComponent(value);
+
+	var updatedCookie = name + "=" + value;
+
+	for (var propName in options) {
+		updatedCookie += "; " + propName;
+		var propValue = options[propName];
+		if (propValue !== true) {
+			updatedCookie += "=" + propValue;
+		}
+	}
+
+	document.cookie = updatedCookie;
+}
+
+function deleteCookie(name) {
+	setCookie(name, "", {
+		expires: -1
+	})
+}
+
+var shadows = {
+	on: function(){
+		$('.shadows').show().addClass('on');
+	},
+	off: function(){
+		setTimeout(function(){
+			$('.shadows').removeClass('on').hide();
+		}, 1500);
+	},
+	on_revert: function(){
+		$('.shadows').show().addClass('on-revert');
+	},
+	off_revert: function(callback){
+		setTimeout(function(){
+			$('.shadows').removeClass('on-revert').addClass('on');
+			callback();
+			setTimeout(function(){
+				$('.shadows').hide();
+			}, 1500);
+		}, 1500);
+	}
+}
